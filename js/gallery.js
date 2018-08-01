562cc234c91ab3e7d4b303277a11d933edac0fbb
technoir9 <roonie9@wp.pl>
2018-08-02 00:20:22 +0200

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,25 @@
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
-  let mainImage = document.querySelector("#gallery-photo img");
+  let mainImage  = document.querySelector("#gallery-photo img");
+  let currentClass = "current";
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
       // Set clicked image as main image
       let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
-      mainImage.setAttribute("alt", thumbnail.dataset.title);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+      // Change which image is current
+      document.querySelector(`.${currentClass}`).classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Update image info
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+      title.innerHTML = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
     });
   });
 }
